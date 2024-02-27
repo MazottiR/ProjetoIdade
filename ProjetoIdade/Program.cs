90da67127d303b11b402d9bc46ce35547f744d16
Usuario <Usuario@CPD03-M06>
2024-02-27 11:35:14 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoIdade/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a sua idade em anos: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua idade em meses: ");
+            int m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite sua idade em dias: ");
+            int d = int.Parse(Console.ReadLine());
+
+            int resultado = (a * 365) + (m * 30) + d;
+
+            Console.WriteLine("Total de dias vividos: " + resultado);
+
+            Console.ReadKey();
+
+        }
+    }
+}
